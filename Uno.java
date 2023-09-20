983bd6bd816e0843560ca58a2bae104c7fb456b9
ManuChamy <manuchamy@gmail.com>
2023-09-20 19:00:04 +0200

    holamundo

---
+++ Uno.java
@@ -0,0 +1,8 @@
+package Uno;
+/*Escribe un programa que imprima "Hola, mundo!" en la consola.*/
+
+public class Uno {
+    public static void main(String[] args) {
+        System.out.println("Hola Mundo");
+    }
+}
